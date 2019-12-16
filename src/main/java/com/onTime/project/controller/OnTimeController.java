80021f8672ab8683aa67b2b674fd6740a6933ca0
choi <sheepoo1@naver.com>
2019-12-16 12:32:41 +0900

    Add : kakao login
    카카오 API를 통한 로그인 기능 테스트 코드 추가

--- src/main/java/com/onTime/project/controller/OnTimeController.java
+++ src/main/java/com/onTime/project/controller/OnTimeController.java
@@ -4,16 +4,18 @@ import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.result.view.RedirectView;
 
 import com.onTime.project.loginAPI.LoginAPI;
 
 @CrossOrigin(origins = "http://localhost:8000")
-@RestController
+@Controller
 public class OnTimeController {
 	@Value("${kakao.key}")
 	private String kakaoKey;
@@ -22,11 +24,12 @@ public class OnTimeController {
 	public LoginAPI loginAPI;
 	
 	@RequestMapping(value="/login")
-	public RedirectView login() {
-		return new RedirectView("redirect:https://kauth.kakao.com/oauth/authorize?client_id="+kakaoKey+"&redirect_uri=http://localhost:8000/oauth&response_type=code");
+	public String login() {
+		return "redirect:https://kauth.kakao.com/oauth/authorize?client_id="+kakaoKey+"&redirect_uri=http://localhost:8000/oauth&response_type=code";
 	}
 	
 	@RequestMapping(value="/oauth")
+	@ResponseBody
 	public String getUserInfo(@RequestParam("code") String code) {
 		System.out.println("aaa");
 	    System.out.println("code : " + code);
