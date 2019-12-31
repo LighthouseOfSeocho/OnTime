a31fc3b94c4bda3cddc19865d7ca076d1d91bd30
sunggookCHOI <sheepoo1@naver.com>
2019-12-31 14:00:41 +0900

    Refactor

--- src/main/java/com/onTime/project/service/OnTimeService.java
+++ src/main/java/com/onTime/project/service/OnTimeService.java
@@ -136,23 +136,17 @@ public class OnTimeService {
 		return users;
 	}
 	
-	public Promise createPromise(Promise promise) {
-		Promise n = null;
+	public boolean createPromise(Promise promise) {
 		try {
 			promiseRepo.save(promise);
 			System.out.println(promise.getPromiseId());
 			promise.setInvitation(sha256(promise.getPromiseId()+""));
 			promiseRepo.save(promise);
 			return true;
-			System.out.println("입력 데이터: "+ promise);
-			n = promiseRepo.save(promise);
-			System.out.println("/데이터 저장 완료");
-//			return n;
 		}catch (Exception e) {
 			e.printStackTrace();
-//			return false;
+			return false;
 		}
-		return n;
 	}
 	
 	public User test() {
