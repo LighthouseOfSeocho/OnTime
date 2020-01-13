d48d3dae26ff65404e68dc1d91c784e7c6ef8f09
awakeng <awaken.gino@gmail.com>
2020-01-13 17:36:07 +0900

    feat : invitation function final

--- src/main/java/com/onTime/project/model/domain/InvitationKey.java
+++
@@ -1,9 +0,0 @@
-package com.onTime.project.model.domain;
-
-import java.io.Serializable;
-
-public class InvitationKey implements Serializable{
-	private String sender;
-	private String recipient;
-	private int promiseId;
-}
