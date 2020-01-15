9f03b800b03b45bca2c36220f30adc221ef21052
LEEJUNGWON1993 <lajuwo@naver.com>
2020-01-15 16:39:45 +0900

    login

--- target/classes/static/JS/test4/app.js
+++ target/classes/static/JS/test4/app.js
@@ -1,7 +1,8 @@
-new Vue({
+let vue = new Vue({
     el: "#app",
     data: { // data 객체
-        mode: "list", // 상태 표시
+        mode: "list", // 상태 표시,
+        user: null,
         memo: {
             id: null,
             title: null,
@@ -12,41 +13,74 @@ new Vue({
         category : "Personal",
         categories : ["Personal", "Business"],
         createPromise : {
-            "promiseName" : '',
-            "roomHostId" : '',
-            "placeName" : '',
-            "placeX" : '',
-            "placeY" : '',
-            "promiseTime" : "2019-12-27 11:43:19.0"
-        }
-        categories : ["Personal", "Business"],
+            "promiseName" : "",
+            "roomHostId" : "",
+            "placeName" : "",
+            "placeX" : "",
+            "placeY" : "",
+            "promiseDate" : "",
+            "promiseTime" : "",
+            "promiseHour" : "",
+            "promiseMinute" : ""
+        },
+        searchedPlaces : null,
+        promises : "",
+        selectedPromise : null,
+        hour : ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
+        minute : ["00", "10", "20", "30", "40", "50"]
     },
     methods: { // methods 객체
+        range: function(start,end){
+            let result = [];
+            for(let i = start ; i<end ; i++){
+                result.push(i);
+            }
+            return result;
+        },
+        setPlace: function(place){
+            this.createPromise.placeName=place.place_name
+            this.createPromise.placeX=place.x
+            this.createPromise.placeY=place.y
+        },
+        selectPromise: function(promise){
+            console.log("Yeaahhhhh")
+            this.selectedPromise=promise
+            this.mode = "promise"
+        },
         renew: function(val) {
             return JSON.parse(JSON.stringify(val)); // JSON.stringify()는 값을 JSON 표기법으로 변환
         },
 
-        Popen: function(title) {
-            for(var i in this.Pmemos) { // id를 가진 memos 찾기
-                if(this.Pmemos[i].title === title) { // id가 같은 memos 찾기
-                    this.memo = this.renew(this.Pmemos[i]); // 자바 스크립트에서 = 의 의미는 객체 일때 단순히 내용을 넣어주는것 뿐만 아니라 참조값으로 들어간다 
+        Popen: function(promiseName) {
+        	console.log(6666666666,promises)
+        	console.log(111111111111, this.mode);
+            for(var i in this.promises) { // id를 가진 memos 찾기
+            	console.log(1231231231, i)
+                if(this.promises[i].promiseName === promiseName) { // id가 같은 memos 찾기
+                    this.promises = this.renew(this.promises[i]); // 자바 스크립트에서 = 의 의미는 객체 일때 단순히 내용을 넣어주는것 뿐만 아니라 참조값으로 들어간다 
                     break; //그래서 Prenew 값만 들어가게 변환해야 한다
                 }
             }
             this.mode = "edit";
+            console.log(11111111,this.promises);
+            console.log(2222222, this.mode);
         },
 
         Pwrite: function() { // 함수 method properties
             this.mode = "write";
-            this.memo = {
-                id: null,
-                title: null,
-                regDate: null
+            this.promises = {
+                "promiseName" : "",
+                "roomHostId" : "",
+                "placeName" : "",
+                "placeX" : "",
+                "placeY" : "",
+                "promiseTime" : "2019-12-27 11:43:19.0"
             }; // 초기화
+            console.log(22222222222,this.promises)
         },
 
         Psave: function() {
-            let id = this.Pmemos.length + 1;
+            let id = this.promises.length + 1;
 
             if(this.mode === "write"){
                 this.Pmemos.push({ // 객체 push
@@ -61,6 +95,7 @@ new Vue({
                         break;
                     }
                 } // 내용 수정 
+                console.log(3333333,this.promises)
             }
 
             this.mode = "list";
@@ -68,16 +103,18 @@ new Vue({
         },
 
         Premove: function() {
+        	console.log(5555555,this.promises)
             if(confirm("정말 삭제하시겠습니까?")){
-                for(var i in this.Pmemos){
-                    if(this.Pmemos[i].id === this.memo.id){
-                        this.Pmemos.splice(i, 1); // 배열 변경 감지, 삭제
+                for(var i in this.promises){
+                    if(this.promises[i].promiseName === this.promises.promiseName){
+                        this.promises.splice(i, 1); // 배열 변경 감지, 삭제
                         break;
                     }
                 }
+                console.log(4444444444,this.promises)
 
                 this.mode = "list";
-                localStorage.setItem("Pmemos", JSON.stringify(this.Pmemos));
+                localStorage.setItem("promises", JSON.stringify(this.promises));
             }
         },
 
@@ -136,21 +173,52 @@ new Vue({
             }
         },
         printInfo: function(query){
-            this.createPromise.roomHostId=query.id;
-            axios.post('/promise', this.createPromise)
+            this.createPromise.roomHostId = query.id;
+            axios.post("/promise", {
+                promiseName : this.createPromise.promiseName,
+                roomHostId : this.createPromise.roomHostId,
+                placeName : this.createPromise.placeName,
+                placeX : this.createPromise.placeX,
+                placeY : this.createPromise.placeY,
+                promiseTime : this.createPromise.promiseDate+" "+this.createPromise.promiseHour+":"+this.createPromise.promiseMinute+":00.0"
+            })
                 .then(res=>{
                     if(res.data){
-                        alert('약속이 생성되었습니다.')
-                    }else{
-                        alert('오류 발생')
+                        alert("약속이 생성되었습니다.");
+                        axios.get("/promise", {params:{userId:this.user.id}})
+                            .then(res=>{
+                                this.promises = res.data;
+                            })
+                            .catch(e=>{
+                                console.log(e);
+                            })
+                    } else {
+                        alert("오류 발생");
                     }
                 }).catch(e=>{
-                    alert(e)
-                })
-        }
+                    alert(e);
+                });
+        },
+        getLocation: function() {
+            if (navigator.geolocation) { // GPS를 지원하면
+              navigator.geolocation.getCurrentPosition(function(position) {
+                alert(position.coords.latitude + ' ' + position.coords.longitude);
+              }, function(error) {
+                console.error(error);
+              }, {
+                enableHighAccuracy: false,
+                maximumAge: 0,
+                timeout: Infinity
+              });
+            } else {
+              alert('GPS를 지원하지 않습니다');
+            }
+          }
     },
 
     created: function() { // vue.js가 가지고 있는 기본 메소드, 앱이 처음 생성될때 실행 되는 부분
+        this.user = query;
+
         let Pmemos = localStorage.getItem("Pmemos");
         let Bmemos = sessionStorage.getItem("Bmemos");
         if(Pmemos) { // 존재 여부
@@ -158,5 +226,12 @@ new Vue({
         } else {
             this.Bmemos = JSON.parse(Bmemos);
         }
+        axios.get("/promise", {params:{userId:this.user.id}})
+            .then(res=>{
+                this.promises = res.data;
+            })
+            .catch(e=>{
+                console.log(e);
+            })
     }
 });
